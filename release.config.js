3c5e46c84962b8702dc9972ba10658af1170cf84
indieisaconcept <me@indieisaconcept.com>
2018-04-29 22:44:31 +1000

    feat(build): build artefacts
    **Product Context**
    
    * Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
      lacinia odio sem nec elit. Sed posuere consectetur est at lobortis.
      Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nullam
      quis risus eget urna mollis ornare vel eu leo.
    
    **Rationale**
    
    * Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
      lacinia odio sem nec elit. Sed posuere consectetur est at lobortis.
      Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nullam
      quis risus eget urna mollis ornare vel eu leo.
    
    **Changes**
    
    * Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
      lacinia odio sem nec elit. Sed posuere consectetur est at lobortis.
    
    * Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nullam
      quis risus eget urna mollis ornare vel eu leo.
    
    * Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nullam
      quis risus eget urna mollis ornare vel eu leo.
    
    **Refs**
    
    * [#123](http://foo.com)
    * [#124](http://foo.com)
    
    `#minor`

--- release.config.js
+++ release.config.js
@@ -1,16 +1,23 @@
 module.exports = {
     npmPublish       : false,
     verifyConditions : [
+        '@semantic-release/changelog',
         '@semantic-release/git',
         '@semantic-release/github'
     ],
-    publish: [
+    prepare : [
+        "@semantic-release/changelog",
+        '@semantic-release/npm',
         {
-            path       : '@semantic-release/npm',
-            tarballDir : 'dist'
+            path : '@semantic-release/exec',
+            cmd  : 'npm run build'
         }, {
-            path   : '@semantic-release/github',
-            assets : 'dist/*.tgz'
+            path    : '@semantic-release/git',
+            assets  : ['package.json', 'dist/**/*.js', 'docs'],
+            message : 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
         }
+    ],
+    publish : [
+        '@semantic-release/github'
     ]
 };
